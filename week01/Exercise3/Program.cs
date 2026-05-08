698b8a2635663c1e9bea77e884741c7ae6ee2f41
hmcch <hcastrochvez@byupathway.edu>
2026-05-07 19:02:31 -0600

    week 01 exercise 3 loops (first core requirement accomplished with if statements)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // C# Programming Exercise 3: Loops
+
+        // Ask the magic number and the user number
+        Console.Write("What is the magic number? ");
+        string computerNumber = Console.ReadLine();
+        int computerNum = int.Parse(computerNumber);
+
+        Console.Write("What is your guess? ");
+        string guessNumber = Console.ReadLine();
+        int guessNum = int.Parse(guessNumber);
+        // Comparing user number against magic/computer number
+        if (guessNum < computerNum)
+        {
+            Console.Write("Higher");
+        }
+        else if (guessNum > computerNum)
+        {
+            Console.Write("Lower");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
     }
 }
